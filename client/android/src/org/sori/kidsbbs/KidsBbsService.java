536981d8737102ef077fefe8d1ed0797bcf3c0b3
Hong Cho <hongcho@gmail.com>
2011-05-13 15:50:36 -0700

    - Reduce the number of articles to keep - Reduce the number of consecutive notifications.

--- client/android/src/org/sori/kidsbbs/KidsBbsService.java
+++ client/android/src/org/sori/kidsbbs/KidsBbsService.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2010, Younghong "Hong" Cho <hongcho@sori.org>.
+// Copyright (c) 2010-2011, Younghong "Hong" Cho <hongcho@sori.org>.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -147,6 +147,7 @@ public class KidsBbsService extends Service
 				mNotificationTitleString,
 				System.currentTimeMillis());
 		mNewArticlesNotification.flags |= Notification.FLAG_AUTO_CANCEL;
+		mNewArticlesNotification.flags |= Notification.FLAG_ONLY_ALERT_ONCE;
 		mNotificationDefaults |= mNewArticlesNotification.defaults;
 		
 		mAlarms = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
