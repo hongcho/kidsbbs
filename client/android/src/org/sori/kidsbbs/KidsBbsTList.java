bc2d45a53f9aaf1ea5adca70de5041b30d92443f
Hong Cho <hongcho@gmail.com>
2011-05-20 12:21:26 -0700

    - toggle read/unread: limit to the currently retrieved seq. - code reorg / cleanup.

--- client/android/src/org/sori/kidsbbs/KidsBbsTList.java
+++ client/android/src/org/sori/kidsbbs/KidsBbsTList.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2010, Younghong "Hong" Cho <hongcho@sori.org>.
+// Copyright (c) 2010-2011, Younghong "Hong" Cho <hongcho@sori.org>.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -25,26 +25,20 @@
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.sori.kidsbbs;
 
-import android.app.AlertDialog;
 import android.content.ContentValues;
-import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.database.Cursor;
 import android.os.Bundle;
 
 public class KidsBbsTList extends KidsBbsAList {
-	private String mTitle;
-	private int mUnreadCount;
 	
     @Override
     public void onCreate(Bundle _state) {
         super.onCreate(_state);
         
-        final Resources resources = getResources();
-        mTitle = resources.getString(R.string.title_tlist);
+        setTitle(getResources().getString(R.string.title_tlist));
+        setQueryBase(KidsBbsProvider.CONTENT_URISTR_TLIST, FIELDS_TLIST, null);
         
         updateTitle();
-        setQueryBase(KidsBbsProvider.CONTENT_URISTR_TLIST, FIELDS_TLIST, null);
         
         registerForContextMenu(getListView());
         
@@ -56,11 +50,11 @@ public class KidsBbsTList extends KidsBbsAList {
     }
     
     protected void updateTitle() {
-    	mUnreadCount = getCount(KidsBbsProvider.CONTENT_URISTR_LIST,
-    			KidsBbsProvider.SELECTION_UNREAD);
-    	final int count = getCount(KidsBbsProvider.CONTENT_URISTR_LIST,
-    			null);
-    	setTitleCommon(mTitle, mUnreadCount, count);
+    	updateTitleCommon(
+    			getCount(KidsBbsProvider.CONTENT_URISTR_LIST,
+    					KidsBbsProvider.SELECTION_UNREAD),
+    			getCount(KidsBbsProvider.CONTENT_URISTR_LIST,
+    					null));
     }
     
     protected boolean matchingBroadcast(int _seq, String _user,
@@ -112,26 +106,6 @@ public class KidsBbsTList extends KidsBbsAList {
     }
     
     protected void toggleAllRead() {
-    	final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setTitle(R.string.confirm_text);
-    	builder.setMessage(R.string.toggle_all_read_message);
-    	builder.setPositiveButton(android.R.string.ok,
-    			new DialogInterface.OnClickListener() {
-    		public void onClick(DialogInterface _dialog, int _which) {
-    			final String where = KidsBbsProvider.KEYA_READ +
-    				(mUnreadCount == 0 ? "!=0" : "=0");
-    			final ContentValues values = new ContentValues();
-    			values.put(KidsBbsProvider.KEYA_READ,
-    					mUnreadCount > 0 ? 1 : 0);
-    			final int nChanged = mResolver.update(getUriList(), values,
-    					where, null);
-    			if (nChanged > 0) {
-    	    		KidsBbs.updateBoardCount(mResolver, mTabname);
-    				refreshList();
-				}
-			}
-    	});
-    	builder.setNegativeButton(android.R.string.cancel, null);
-    	builder.create().show();
+    	toggleAllReadCommon("");
     }
 }
