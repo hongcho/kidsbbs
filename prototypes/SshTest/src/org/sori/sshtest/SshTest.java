c14e63e38e8460e35ac30b06a854efda41bc175a
Hong Cho <hongcho@gmail.com>
2011-12-27 15:57:08 -0800

    - For prototyping telnet/ssh tests.

---
+++ prototypes/SshTest/src/org/sori/sshtest/SshTest.java
@@ -0,0 +1,426 @@
+package org.sori.sshtest;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SshTest extends Activity {
+	public static final String TAG = "SshTest";
+
+	private TextView mTextView;
+	private SshTask mLastUpdate = null;
+
+	private String mUsername;
+	private String mPassword;
+	private String mPostTitle;
+	private String mPostBody;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mTextView = (TextView)findViewById(R.id.text);
+
+		final Resources resources = getResources();
+		mUsername = resources.getString(R.string.username);
+		mPassword = resources.getString(R.string.password);
+		mPostTitle = resources.getString(R.string.post_title);
+		mPostBody = resources.getString(R.string.post_body);
+
+		mLastUpdate = new SshTask();
+		mLastUpdate.execute();
+	}
+
+	private class SshTask extends AsyncTask<Void, Integer, Void> {
+		private static final String S_EUCKR = "EUC-KR";
+
+		// Kids BBS states
+		private static final int ST_USER = 0;
+		private static final int ST_PASSWORD = 1;
+		private static final int ST_CLOSE_OTHERS = 2;
+		private static final int ST_GUEST_LOGIN = 3;
+
+		private static final int ST_LOGIN_NOTICE = 9;
+		private static final int ST_MENU_KIDS = 10;
+		private static final int ST_MENU_BBS = 11;
+		private static final int ST_MENU_WRITER = 12;
+		private static final int ST_MENU_SQUARE = 13;
+
+		private static final int ST_LIST_BBS = 20;
+		private static final int ST_VIEW_BBS = 21;
+		private static final int ST_DELETE_BBS = 22;
+
+		private static final int ST_WRITE_BBS = 30;
+		private static final int ST_WRITE_BBS_BODY = 31;
+		private static final int ST_WRITE_BBS_END = 32;
+
+		private static final int ST_QUIT = 99;
+		private static final int ST_INVALID = -1;
+
+		private static final int ST_LIST_BBS_1 = 100; // temp
+		private static final int ST_MENU_BBS_1 = 101; // temp
+		
+		// Patterns for prompts.
+		private final Pattern P_USER =
+			Pattern.compile("\\(User  Id\\):");
+		private final Pattern P_PASSWORD =
+			Pattern.compile("\\(Password\\):");
+		private final Pattern P_GUEST_NAME =
+			Pattern.compile("^I Am ");
+		private final Pattern P_CLOSE_OTHERS =
+			Pattern.compile("\\(y or n\\)\\[y\\]");
+		private final Pattern P_LESS_STATUS =
+			Pattern.compile("^\\((\\d+)%\\) CTRL");
+		private final Pattern P_MUTT_STATUS =
+			Pattern.compile(" -- \\((\\d+)%|(all|end)\\)\\s*$");
+		private final Pattern P_MENU_HEADER =
+			Pattern.compile("^<[<>] ");
+		private final Pattern P_MENU_PROMPT =
+			Pattern.compile("^\\* ");
+		private final Pattern P_LIST_PROMPT =
+			Pattern.compile("^>\\s*\\d+");
+		private final Pattern P_WRITE_TITLE =
+			Pattern.compile(": ");
+		private final Pattern P_WRITE_FOOTER_S =
+			Pattern.compile("\\[ New file \\]");
+		private final Pattern P_WRITE_FOOTER_V =
+			Pattern.compile("\\[NEW FILE\\]");
+		private final Pattern P_WRITE_SIG =
+			Pattern.compile(" <Y, n> ");
+		private final Pattern P_WRITE_S_PAT =
+			Pattern.compile("(?<=\\n)\\n");
+		private static final String S_WRITE_S_REP =
+			"\r\n";
+
+		private KidsConnection mConn = new KidsConnection();
+
+		private int handleLogin(int _state) throws IOException {
+			switch (_state) {
+			case ST_USER:
+				return handleUser(_state);
+			case ST_PASSWORD:
+				return handlePassword(_state);
+			case ST_CLOSE_OTHERS:
+				return handleCloseOthers(_state);
+			case ST_GUEST_LOGIN:
+				return handleGuestLogin(_state);
+			default:
+				throw new IOException("Login: unknown state " + _state);
+			}
+		}
+
+		private int handleUser(int _state) throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			if (P_USER.matcher(s).find()) {
+				mConn.write(mUsername + "\n");
+				if (mUsername.equals("guest")) {
+					return ST_GUEST_LOGIN;
+				} else {
+					return ST_PASSWORD;
+				}
+			}
+			return _state;
+		}
+
+		private int handlePassword(int _state) throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			if (P_PASSWORD.matcher(s).find()) {
+				mConn.write(mPassword + "\n");
+				return ST_CLOSE_OTHERS;
+			}
+			return _state;
+		}
+
+		private int handleCloseOthers(int _state) throws IOException {
+			String s = mConn.getLine(mConn.getY() - 1, S_EUCKR);
+			if (P_CLOSE_OTHERS.matcher(s).find()) {
+				mConn.write("n\n");
+				return ST_LOGIN_NOTICE;
+			}
+			// Check if there were no other logins.
+			s = mConn.getCurrentLine(S_EUCKR);
+			if (P_LESS_STATUS.matcher(s).find()) {
+				return ST_LOGIN_NOTICE;
+			}
+			if (P_MUTT_STATUS.matcher(s).find()) {
+				return ST_LOGIN_NOTICE;
+			}
+			return _state;
+		}
+
+		private int handleGuestLogin(int _state) throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			if (P_GUEST_NAME.matcher(s).find()) {
+				mConn.write(mUsername + "\n");
+				mConn.write("Y\n");
+				mConn.write("vt100\n");
+				return ST_LOGIN_NOTICE;
+			} else {
+				throw new IOException("GuestLogin: unexpected prompt");
+			}
+		}
+
+		private int handleArticle(int _state, int _next) throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			Matcher m = P_LESS_STATUS.matcher(s);
+			if (m.find()) {
+				int n = Integer.parseInt(m.group(1));
+				if (n >= 100) {
+					mConn.write("q");
+					return _next;
+				} else {
+					mConn.write(" ");
+				}
+				return _state;
+			}
+			m = P_MUTT_STATUS.matcher(s);
+			if (m.find()) {
+				int n;
+				try {
+					n = Integer.parseInt(m.group(1));
+				} catch (NumberFormatException e1) {
+					n = -1;
+				}
+				if (n < 0) {
+					mConn.write("q");
+					return _next;
+				} else {
+					mConn.write(" ");
+				}
+				return _state;
+			}
+			return _state;
+		}
+
+		private int handleWriteBbs(int _state, int _next, String _body)
+				throws IOException {
+			switch (_state) {
+			case ST_WRITE_BBS:
+				return handleWriteBbsTitle(_state);
+			case ST_WRITE_BBS_BODY:
+				return handleWriteBbsBody(_state, _body);
+			case ST_WRITE_BBS_END:
+				return handleWriteBbsEnd(_state, _next);
+			default:
+				throw new IOException("WriteBbs: unknown state " + _state);
+			}
+		}
+
+		private int handleWriteBbsTitle(int _state) throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			if (P_WRITE_TITLE.matcher(s).find()) {
+				mConn.write(mPostTitle + "\n");
+				return ST_WRITE_BBS_BODY;
+			}
+			return _state;
+		}
+
+		private int handleWriteBbsBody(int _state, String _body)
+				throws IOException {
+			String s = mConn.getLine(mConn.getHeight() - 1, S_EUCKR);
+			if (P_WRITE_FOOTER_S.matcher(s).find()) {
+				mConn.write(
+						P_WRITE_S_PAT.matcher(_body).replaceAll(S_WRITE_S_REP));
+				mConn.write("\030"); // Ctrl-X
+				return ST_WRITE_BBS_END;
+			}
+			if (P_WRITE_FOOTER_V.matcher(s).find()) {
+				mConn.write("i"); // insert
+				mConn.write(_body);
+				mConn.write("\033ZZ"); // ESC ZZ
+				return ST_WRITE_BBS_END;
+			}
+			return _state;
+		}
+
+		private int handleWriteBbsEnd(int _state, int _next) throws IOException {
+			String s = mConn.getLine(0, S_EUCKR);
+			if (P_WRITE_SIG.matcher(s).find()) {
+				mConn.write("n\n"); // signature?
+				mConn.write("n\n"); // save?
+				mConn.write("\n"); // return.
+				return _next;
+			}
+			return _state;
+		}
+
+		private int handleMenuKids(int _state, int _next) throws IOException {
+			String s0 = mConn.getLine(0, S_EUCKR);
+			String s1 = mConn.getLine(1, S_EUCKR);
+			if (P_MENU_HEADER.matcher(s0).find()
+					&& P_MENU_PROMPT.matcher(s1).find()) {
+				switch (_next) {
+				case ST_MENU_BBS:
+					mConn.write("b\n");
+					return _next;
+				case ST_MENU_WRITER:
+					mConn.write("w\n");
+					return _next;
+				case ST_MENU_SQUARE:
+					mConn.write("s\n");
+					return _next;
+				case ST_QUIT:
+					mConn.write("q\ny");
+					return _next;
+				default:
+					throw new IOException(
+							"MenuKids: unknown next state: " + _next);
+				}
+			}
+			return _state;
+		}
+
+		private int handleMenuBbs(int _state, int _next, String _board)
+				throws IOException {
+			String s0 = mConn.getLine(0, S_EUCKR);
+			String s1 = mConn.getLine(1, S_EUCKR);
+			if (P_MENU_HEADER.matcher(s0).find()
+					&& P_MENU_PROMPT.matcher(s1).find()) {
+				switch (_next) {
+				case ST_LIST_BBS:
+					mConn.write("s\n");
+					mConn.write(_board + "\n");
+					mConn.write("r\n");
+					return _next;
+				case ST_MENU_BBS:
+				case ST_MENU_BBS_1:
+					mConn.write("p");
+					return _next;
+				case ST_QUIT:
+					mConn.write("q\ny");
+					return _next;
+				default:
+					throw new IOException(
+							"MenuBbs: unknown next state: " + _next);
+				}
+			}
+			return _state;
+		}
+
+		private int handleListBbs(int _state, int _next, String _pos)
+				throws IOException {
+			String s = mConn.getCurrentLine(S_EUCKR);
+			if (P_LIST_PROMPT.matcher(s).find()) {
+				switch (_next) {
+				case ST_VIEW_BBS:
+					mConn.write(_pos + "\n ");
+					return _next;
+				case ST_DELETE_BBS:
+					mConn.write(_pos + "\nd");
+					return _next;
+				case ST_WRITE_BBS:
+					mConn.write("w");
+					return _next;
+				case ST_MENU_BBS:
+				case ST_MENU_BBS_1:
+					mConn.write("q");
+					return _next;
+				default:
+					throw new IOException(
+							"ListBbs: unknown next state: " + _next);
+				}
+			}
+			return _state;
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+			int state = ST_USER;
+			try {
+				mConn.open();
+				while (true) {
+					try {
+						mConn.process();
+					} catch (KidsConnection.TimeoutException e) {
+						publishProgress(state);
+						switch (state) {
+						case ST_USER:
+						case ST_PASSWORD:
+						case ST_CLOSE_OTHERS:
+						case ST_GUEST_LOGIN:
+							state = handleLogin(state);
+							break;
+						case ST_LOGIN_NOTICE:
+							state = handleArticle(state, ST_MENU_KIDS);
+							break;
+						case ST_MENU_KIDS:
+							state = handleMenuKids(state, ST_MENU_BBS);
+							break;
+						case ST_MENU_BBS:
+							state = handleMenuBbs(state, ST_LIST_BBS,
+									"Stanford");
+							break;
+						case ST_LIST_BBS:
+							//state = handleListBbs(state, ST_VIEW_BBS, "1000");
+							state = handleListBbs(state, ST_WRITE_BBS, null);
+							break;
+						case ST_VIEW_BBS:
+							state = handleArticle(state, ST_LIST_BBS_1);
+							break;
+						case ST_WRITE_BBS:
+						case ST_WRITE_BBS_BODY:
+						case ST_WRITE_BBS_END:
+							state = handleWriteBbs(state, ST_LIST_BBS_1, mPostBody);
+							break;
+
+						case ST_LIST_BBS_1:
+							state = handleListBbs(state, ST_MENU_BBS_1, null);
+							break;
+						case ST_MENU_BBS_1:
+							state = handleMenuBbs(state, ST_QUIT, null);
+							break;
+
+						case ST_QUIT:
+							// Wait until EOF.
+							break;
+						case ST_INVALID:
+						default:
+							throw e;
+						}
+					} catch (KidsConnection.EOFException e) {
+						break;
+					}
+				}
+			} catch (IOException e) {
+				Log.e(TAG, "IOException", e);
+			}
+			publishProgress(state);
+			mConn.close();
+			return null;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			mTextView.setText("");
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... _args) {
+			String t = "";
+			try {
+				t += mConn.dump();
+				t += "====== " + _args[0] + " [" + mConn.getY() + ":"
+					+ mConn.getX() + "] ======\n";
+				t += mConn.dumpCurrentLine();
+			} catch (IOException e) {
+				t += (String)mTextView.getText();
+				t += "\n[[[dump failed]]]";
+			}
+			mTextView.setText(t);
+		}
+
+		@Override
+		protected void onPostExecute(Void _result) {
+			String t = (String)mTextView.getText();
+			mTextView.setText(t + "[[[DONE]]]");
+		}
+	}
+}
---
+++ prototypes/SshTest/src/org/sori/sshtest/VT220Screen.java
@@ -0,0 +1,418 @@
+// Copyright (c) 2011, Younghong "Hong" Cho <hongcho@sori.org>.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//   1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//   2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//   3. Neither the name of the organization nor the names of its contributors
+// may be used to endorse or promote products derived from this software
+// without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+package org.sori.sshtest;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.util.Log;
+
+public class VT220Screen {
+	private static final String TAG = "VT220Screen";
+
+	private static final String EUC_KR = "EUC-KR";
+	private static final int BUF_SIZE = 40;
+	private static final int ESCBUF_SIZE = 40;
+
+	// Escape (mostly CSI) parameter patterns
+	private final Pattern P_ARG_SINGLE =
+		Pattern.compile("^\\[(\\d+)$");
+	private final Pattern P_ARG_DOUBLE =
+		Pattern.compile("^\\[(\\d+);(\\d+)$");
+	private final Pattern P_ARG_012 =
+		Pattern.compile("^\\[([012])$");
+
+	private int mWidth = 80;
+	private int mHeight = 24;
+
+	private int mX = 0;
+	private int mY = 0;
+
+	private byte[][] mScrBuf;
+	private byte[] mBlankLine;
+
+	private int mEscState = 0;
+	private byte[] mEscBuf = new byte[ESCBUF_SIZE];
+	private int mEscBufLen = 0;
+
+	// Circular buffer.
+	private byte[] mInputBuf = new byte[BUF_SIZE];
+	private int mInputBufWr = 0;
+	private int mInputBufRd = 0;
+
+	public int getWidth() {
+		return mWidth;
+	}
+
+	public int getHeight() {
+		return mHeight;
+	}
+
+	public int getX() {
+		return mX;
+	}
+
+	public int getY() {
+		return mY;
+	}
+
+	public VT220Screen(int _width, int _height) {
+		mWidth = _width;
+		mHeight = _height;
+
+		mScrBuf = new byte[mHeight][mWidth];
+		mBlankLine = new byte[mWidth];
+		for (int i = 0; i < mWidth; ++i) {
+			mBlankLine[i] = ' ';
+		}
+
+		clear();
+
+		mEscState = 0;
+		mEscBufLen = 0;
+	}
+
+	public void process(byte[] _buf, int _len) {
+		Matcher m;
+		int n, x, y;
+		L_MAIN: for (int i = 0; i < _len; ++i) {
+			byte v = _buf[i];
+
+			// Keep X, Y in check
+			adjustX();
+			adjustY();
+
+			// ESC parsing
+			switch (mEscState) {
+			case 0:
+				switch (v) {
+				case 13: // CR
+					mX = 0;
+					continue L_MAIN;
+				case 10: // LF
+					++mY;
+					continue L_MAIN;
+				case 27: // ESC
+					mEscBufLen = 0;
+					mEscState = 1;
+					continue L_MAIN;
+				case 8: // BS
+					--mX;
+					continue L_MAIN;
+				case 12: // FF
+					clear();
+					continue L_MAIN;
+				default:
+					if ((0 <= v && v < 32) || 127 == v) { // CNTRL
+						continue L_MAIN;
+					}
+					break;
+				}
+				break;
+			case 1: // First character after ESC
+				switch (v) {
+				case '[':
+					mEscBuf[mEscBufLen++] = v;
+					mEscState = 2;
+					break;
+				case '#':
+					mEscBuf[mEscBufLen++] = v;
+					mEscState = 3;
+					break;
+				case 'D':
+					++mY;
+					mEscState = 0;
+					break;
+				case 'M':
+					--mY;
+					mEscState = 0;
+					break;
+				case 'E':
+					++mY;
+					mX = 0;
+					mEscState = 0;
+					break;
+				case '(': case ')': case '*': case '+': case ' ':
+					mEscBuf[mEscBufLen++] = v;
+					mEscState = 4;
+					break;
+				case 'c': case 'N': case 'O': case 'P': case 'H':
+				case '^': case '_': case ']': case '7': case '8':
+				default:
+					// Consume a character.
+					mEscState = 0;
+					break;
+				}
+				continue L_MAIN;
+			case 2: // ESC [
+				switch (v) {
+				case 'A': case 'B': case 'C': case 'D':
+					n = 1;
+					m = P_ARG_SINGLE
+							.matcher(new String(mEscBuf, 0, mEscBufLen));
+					if (m.find()) {
+						n = Integer.parseInt(m.group(1));
+					}
+					// They all stop at the borders. No scrolling.
+					switch (v) {
+					case 'A':
+						mY -= n;
+						if (mY < 0) {
+							mY = 0;
+						}
+						break;
+					case 'B':
+						mY += n;
+						if (mY >= mHeight) {
+							mY = mHeight - 1;
+						}
+						break;
+					case 'C':
+						mX += n;
+						if (mX < 0) {
+							mX = 0;
+						}
+						break;
+					case 'D':
+						mX -= n;
+						if (mX >= mWidth) {
+							mX = mWidth - 1;
+						}
+						break;
+					}
+					mEscState = 0;
+					break;
+				case 'H': case 'f':
+					x = 1;
+					y = 1;
+					m = P_ARG_DOUBLE
+							.matcher(new String(mEscBuf, 0, mEscBufLen));
+					if (m.find()) {
+						x = Integer.parseInt(m.group(2));
+						y = Integer.parseInt(m.group(1));
+					}
+					mX = x - 1;
+					mY = y - 1;
+					mEscState = 0;
+					break;
+				case 'K': case 'J':
+					n = 0;
+					m = P_ARG_012.matcher(new String(mEscBuf, 0, mEscBufLen));
+					if (m.find()) {
+						n = Integer.parseInt(m.group(1));
+					}
+					switch (v) {
+					case 'K':
+						switch (n) {
+						case 0: clearLine(mY, mX, mWidth - mX); break;
+						case 1: clearLine(mY, 0, mX + 1); break;
+						case 2: clearLine(mY, 0, mWidth); break;
+						}
+						break;
+					case 'J':
+						switch (n) {
+						case 0: clearLines(mY, mHeight - mY); break;
+						case 1: clearLines(0, mY + 1); break;
+						case 2: clear(); break;
+						}
+						break;
+					}
+					mEscState = 0;
+					break;
+				case 'L': // insert lines
+				case 'M': // delete lines
+				case '@': // insert blanks (no cursor change)
+				case 'P': // delete characters
+				case 'X': // erase characters (no shifting)
+				case 'r': // set scroll margin
+				case 'p': case 'h': case 'l': case 'g': case 'm':
+				case 'q': case 'i': case 'c': case 'n': case 'R':
+				case 'y': case 's': case 'u': case 'b': case 'G':
+				case 'd':
+					// Ignored
+					mEscState = 0;
+					break;
+				default:
+					mEscBuf[mEscBufLen++] = v;
+					break;
+				}
+				continue L_MAIN;
+			case 3: // Consume a byte.
+				mEscState = 0;
+				continue L_MAIN;
+			case 4: // Character sets
+				if (0x30 <= v && v <= 0x7E) {
+					mEscState = 0;
+				} else if (0x20 <= v && v <= 0x2F) {
+					mEscBuf[mEscBufLen++] = v;
+				} else {
+					// Unknown: backup and retry
+					--i;
+					mEscState = 0;
+				}
+				continue L_MAIN;
+			default:
+				Log.e(TAG, "Unknown state: mEscState = " + mEscState);
+				break;
+			}
+
+			// Now put it in the screen
+			mScrBuf[mY][mX++] = v;
+
+			// Circular input buffer
+			mInputBuf[mInputBufWr++] = v;
+			mInputBufWr %= BUF_SIZE;
+			if (mInputBufWr == mInputBufRd) {
+				mInputBufRd = (mInputBufRd + 1) % BUF_SIZE;
+			}
+		}
+
+		// Keep X, Y in check
+		adjustX();
+		adjustY();
+	}
+
+	public int getInputBuf(byte[] _buf) {
+		int size = _buf.length;
+		if (mInputBufRd == mInputBufWr) {
+			return 0;
+		} else if (mInputBufRd < mInputBufWr) {
+			int len = mInputBufWr - mInputBufRd;
+			if (len > size) {
+				len = size;
+			}
+			System.arraycopy(mInputBuf, mInputBufRd, _buf, 0, len);
+			return len;
+		} else {
+			int len0 = BUF_SIZE - mInputBufRd;
+			if (len0 > size) {
+				len0 = size;
+			}
+			if (len0 > 0) {
+				System.arraycopy(mInputBuf, mInputBufRd, _buf, 0, len0);
+				size -= len0;
+			}
+			int len1 = mInputBufWr;
+			if (len1 > size) {
+				len1 = size;
+			}
+			if (len1 > 0) {
+				System.arraycopy(mInputBuf, 0, _buf, len0, len1);
+			}
+			return len0 + len1;
+		}
+	}
+
+	public void consumeInputBuf(int _size) {
+		mInputBufRd = (mInputBufRd + _size) % BUF_SIZE;
+	}
+
+	public byte[] getLine(int _y) {
+		return mScrBuf[_y];
+	}
+
+	public String dumpLine(int _y) throws IOException {
+		return String.format("%02d", _y) + ": "
+			+ new String(mScrBuf[_y], 0, mWidth, EUC_KR) + "\n";
+	}
+
+	public String dump() throws IOException {
+		String d = "";
+		for (int i = 0; i < mHeight; ++i) {
+			d += dumpLine(i);
+		}
+		return d;
+	}
+
+	private void clearLine(int _y, int _off, int _len) {
+		System.arraycopy(mBlankLine, _off, mScrBuf[_y], _off, _len);
+	}
+
+	private void clearLines(int _start, int _count) {
+		int last = _start + _count;
+		for (int i = _start; i < last; ++i) {
+			clearLine(i, 0, mWidth);
+		}
+	}
+
+	private void clear() {
+		clearLines(0, mHeight);
+		mX = 0;
+		mY = 0;
+	}
+
+	private void scrollUp(int _n) {
+		if (_n >= mHeight) {
+			clear();
+		} else if (_n > 0) {
+			for (int i = 0; i < mHeight - _n; ++i) {
+				System.arraycopy(mScrBuf[i + _n], 0, mScrBuf[i], 0, mWidth);
+			}
+			clearLines(mHeight - _n, _n);
+		}
+	}
+
+	private void scrollDown(int _n) {
+		if (_n >= mHeight) {
+			clear();
+		} else if (_n > 0) {
+			for (int i = mHeight - _n - 1; i >= 0; --i) {
+				System.arraycopy(mScrBuf[i], 0, mScrBuf[i + _n], 0, mWidth);
+			}
+			clearLines(0, _n);
+		}
+	}
+
+	private void adjustX() {
+		if (mX < 0) {
+			mX = 0;
+		} else {
+			while (mX >= mWidth) {
+				++mY;
+				mX -= mWidth;
+			}
+		}
+	}
+
+	private void adjustY() {
+		if (mY < 0) {
+			int n = -mY;
+			if (n > mHeight) {
+				n = mHeight;
+			}
+			scrollDown(n);
+			mY = 0;
+		} else if (mY >= mHeight) {
+			int n = mY - mHeight + 1;
+			if (n > mHeight) {
+				n = mHeight;
+			}
+			scrollUp(n);
+			mY = mHeight - 1;
+		}
+	}
+}
